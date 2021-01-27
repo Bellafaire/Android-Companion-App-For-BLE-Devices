eedf032da23bfe95b6cdd7ff0c1a42da5ee34ad1
Matthew Bellafaire <matthewjbella@gmail.com>
2021-01-27 11:45:23 -0500

    re-added notification reading

--- SmartwatchCompanionAppV2/app/src/main/java/com/example/smartwatchcompanionappv2/MainActivity.java
+++ SmartwatchCompanionAppV2/app/src/main/java/com/example/smartwatchcompanionappv2/MainActivity.java
@@ -10,7 +10,10 @@ Background operation of BLE Library with Android 8 - Request for Example: https:
  */
 
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,10 +28,15 @@ public class MainActivity extends AppCompatActivity {
     public static String serviceUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
     public static String charUUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
+    public static String notificationData = "";
+
     private static String TAG = "Main";
     public static BLEGATT blegatt;
     public static String[] tabText = {"First Tab", "Second Tab"};
     public static TextView txtView;
+
+    private NotificationReceiver nReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
         txtView.setText("Test");
         reference = this;
 
+        //init notification receiver
+        nReceiver = new NotificationReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(NLService.NOTIFICATION_ACTION);
+        registerReceiver(nReceiver, filter);
+
+        //get the current notifications by broadcasting an intent
+        Intent i = new Intent(NLService.GET_NOTIFICATION_INTENT);
+        i.putExtra("command", "list");
+        sendBroadcast(i);
+
+
+
         BLEScanner.startScan(this.getApplicationContext());
         blegatt = new BLEGATT(this.getApplicationContext(), (BluetoothManager) this.getSystemService(Context.BLUETOOTH_SERVICE));
     }
@@ -46,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
         reference.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                String statusText = BLEGATT.getStatusText();
+                String statusText = BLEGATT.getStatusText()
+                        +"\nNotification Data: \n" + notificationData;
                 reference.txtView.setText(statusText);
             }
         });
@@ -61,5 +83,21 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "SENDING DATE AND TIME");
         blegatt.write(BLEGATT.getDateAndTime());
     }
+
+    //receives the data from the NLService and updates fields in this class.
+    class NotificationReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onRecieve method callback received " + intent.getStringExtra("notification_event"));
+            String temp = intent.getStringExtra("notification_event");
+            if (!notificationData.contains(temp)) {
+                temp = intent.getStringExtra("notification_event") + "\n" + notificationData;
+                notificationData = temp.replace("\n\n", "\n");
+            }
+updateStatusText();
+        }
+    }
+
+
 
 }
