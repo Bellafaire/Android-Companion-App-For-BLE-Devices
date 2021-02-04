80638b366afd7c357b421d1ea066df960a3643f9
Matthew Bellafaire <matthewjbella@gmail.com>
2021-02-04 01:33:59 -0500

    Changed ESP32 example to use blocking sendBLE command (for backwards compatibility mostly)

--- ESP32_BLE_Example_Code_V2/BLEServer.ino
+++ ESP32_BLE_Example_Code_V2/BLEServer.ino
@@ -1,8 +1,51 @@
+/*****************************************************************************
+  The MIT License (MIT)
+  Copyright (c) 2021 Matthew James Bellafaire
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+******************************************************************************/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+//variables and defines used by BLEServer.ino
+String currentDataField;
+#define SERVICE_UUID        "5ac9bc5e-f8ba-48d4-8908-98b80b566e49"
+#define COMMAND_UUID        "bcca872f-1a3e-4491-b8ec-bfc93c5dd91a"
+BLECharacteristic *commandCharacteristic;
+
+//indicates connection state to the android device
+boolean connected = false;
+
+//indiciates whether or not a operation is currently in progress
+boolean operationInProgress = false;
+
+//function signitures
+String sendBLE(String command);
+void addData(String data);  //adds data to a current string, used within BLEServer.ino
+void initBLE(); //initializes the BLE connection by starting advertising.
+
+
+
 
 void addData(String data) {
-  //  Serial.println("Adding:" + data);
-  currentDataField->concat(data);
-
+//  Serial.println("Adding:" + data);
+  currentDataField += data;
 }
 
 class cb : public BLEServerCallbacks    {
@@ -18,10 +61,9 @@ class ccb : public BLECharacteristicCallbacks  {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
       addData(String( pCharacteristic->getValue().c_str()));
-      operationInProgress = true;
     }
     void onRead(BLECharacteristic* pCharacteristic) {
-//      Serial.println("Characteristic Read");
+      //      Serial.println("Characteristic Read");
       operationInProgress = false;
     }
 };
@@ -59,35 +101,17 @@ void initBLE() {
   BLEDevice::startAdvertising();
 }
 
-boolean sendBLE(String command, String* returnString) {
-  if (connected && !operationInProgress) {
-    commandCharacteristic->setValue(command.c_str());
-    commandCharacteristic->notify();
+String sendBLE(String command) {
+  operationInProgress = true;
+  commandCharacteristic->setValue(command.c_str());
+  commandCharacteristic->notify();
 
-    *returnString = "";
-    currentDataField = returnString;
+  currentDataField = "";
 
-    unsigned long startTime = millis();
-    while (!operationInProgress && (startTime + 100 < millis()))
-      delay(10);
+  unsigned long startTime = millis();
+  while (operationInProgress && (startTime + 2000 > millis()))
+    delay(25);
 
-    return true;
-  } else {
-    return false;
-  }
-}
+  return currentDataField;
 
-boolean sendBLE(String command) {
-  if (connected && !operationInProgress) {
-    commandCharacteristic->setValue(command.c_str());
-    commandCharacteristic->notify();
-
-    unsigned long startTime = millis();
-    while (!operationInProgress && (startTime + 100 < millis()))
-      delay(10);
-
-    return true;
-  } else {
-    return false;
-  }
 }
--- ESP32_BLE_Example_Code_V2/ESP32_BLE_Example_Code_V2.ino
+++ ESP32_BLE_Example_Code_V2/ESP32_BLE_Example_Code_V2.ino
@@ -1,35 +1,22 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
-
-//variables and defines used by BLEServer.ino
-String* currentDataField;
-#define SERVICE_UUID        "5ac9bc5e-f8ba-48d4-8908-98b80b566e49"
-#define COMMAND_UUID        "bcca872f-1a3e-4491-b8ec-bfc93c5dd91a"
-BLECharacteristic *commandCharacteristic;
-
-//indicates connection state to the android device
-boolean connected = false;
-
-//indiciates whether or not a operation is currently in progress
-boolean operationInProgress = false;
-
-//function signitures
-boolean sendBLE(String command, String* returnString); //sends command to the android device and puts returned data into the string indicated by a given pointer (returns true if command was issued successfully)
-boolean sendBLE(String command); //sends command to the android device without return data (returns true if command was issued successfully)
-void addData(String data);  //adds data to a current string, used within BLEServer.ino
-void initBLE(); //initializes the BLE connection by starting advertising.
-
-
-//strings declared for use in this example
-String notificationData = "";
-String timeData = "";
-String spotifyStatus = "";
-String spotifySong = "";
-String calendarData = "";
-
-
+/*****************************************************************************
+  The MIT License (MIT)
+  Copyright (c) 2021 Matthew James Bellafaire
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+******************************************************************************/
 void setup() {
   Serial.begin(115200);
   initBLE();
@@ -39,6 +26,7 @@ void loop() {
   //available commands
   //  sendBLE("/notifications", &string); //gets current android notifications as a string format "appName,Title;ExtraText,ExtraInfoText,ExtraSubText,ExtraTitle;Description;"
   //  sendBLE("/calendar", &string); // returns a string of calender events for the next 24 hours in format "title;description;startDate;startTime;endTime;eventLocation;"
+  //  sendBLE("/time", &string); // returns a string representing the time
   //  sendBLE("/isPlaying", &string); //returns "true" or "false" indicating whether spotify is playing on the android device
   //  sendBLE("/currentSong", &string); //returns the current song name and artist playing on spotify as one string
   //  sendBLE("/play"); //hits the media play button on the android device
@@ -46,11 +34,6 @@ void loop() {
   //  sendBLE("/nextSong"); //hits the media next song button on the android device
   //  sendBLE("/lastSong"); //hits the media previous song button on the android device
 
-  if (connected && !operationInProgress) {
-    //commands submitted will take some time to be completed. In that time the data recieved from the android device is placed into the
-    // string indicated by the pointer, other operations are allowed to happen during this time
-    sendBLE("/calendar", &notificationData);
-  }
+  Serial.println(sendBLE("/time"));
   delay(1000);
-  Serial.println(notificationData);
 }
